47ab233814c844e1dc9eece860a54369bd734d8f
kamenon2 <kamenon2@illinois.edu>
2019-01-29 12:24:27 -0600

    initial commit

--- cs498_applied_machine_learning/hw2a-normal.py
+++ cs498_applied_machine_learning/hw2a-normal.py
@@ -1,10 +1,23 @@
+'''
+The coding for this excerciese is completed using multiple libraries and sample implementation 
+found online and clubbed together as per the exercise requirements.
+'''
+
 import numpy as np
 import pandas as pd
 import csv, codecs, random, math
 from mnist import MNIST
 
-mndata = MNIST('data')
-trn_images, trn_labels = mndata.load_training()
-tst_images, tst_labels = mndata.load_testing()
+#The mnist 
+def downldAndExtrctDigitFiles():
+    
+def readMnistDigitData():
+    mndata = MNIST('data')
+    trn_images, trn_labels = mndata.load_training()
+    tst_images, tst_labels = mndata.load_testing()
+    print (trn_images)
 
-print (trn_images)
+def main():
+
+
+main()
